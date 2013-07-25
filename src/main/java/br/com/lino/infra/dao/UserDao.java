b37de33ec00fe1367167b2d8b5ad4ac938749119
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-07-25 13:24:36 -0300

    concertando os test

--- src/main/java/br/com/lino/infra/dao/UserDao.java
+++ src/main/java/br/com/lino/infra/dao/UserDao.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Component;
 
 import br.com.lino.domain.model.User;
 import br.com.lino.domain.repository.UserRepository;
+import br.com.lino.infra.annotation.AddDependencies;
 
 @Component
 public class UserDao implements UserRepository {
 
+	@AddDependencies
 	public User load(Long id) {
 		return new User(1L);
 	}
 
+	@AddDependencies
 	public List<User> list() {
 		return Arrays.asList(new User(1L), new User(2L));
 	}
